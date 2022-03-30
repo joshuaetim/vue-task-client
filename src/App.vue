2efecdd5dde3f1e97ab5981723a9fcbb9bc9afad
Joshua Etim <jetimworks@gmail.com>
2022-03-30 04:54:39 +0100

    removed default view pt1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <h1>Hello World</h1>
   <router-view/>
 </template>
 
